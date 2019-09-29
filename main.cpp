c9360a7e1a2a908cbac0cc4a3244e667494762e4
Eduardo Quirós <eduardo@eduardoquiros.com>
2019-09-29 17:00:32 -0600

    Hacer pequeño ejemplo.
    Signed-off-by: Eduardo Quirós <eduardo@eduardoquiros.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <string.h>
+#include "persona.cpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Persona pedro, alicia;
+    alicia = *new Persona("Alicia",int(22),"Costarricense");
+    pedro = *new Persona("Pedro",int(24),"Peruano");
+
+    string intercambio = alicia.saludar(pedro);
+
+    cout << intercambio << endl;
+
     return 0;
 }
